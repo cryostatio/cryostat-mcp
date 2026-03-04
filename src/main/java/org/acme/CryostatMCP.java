33aea136c744dd941dbb1aa6cace494cb7ca8b27
Andrew Azores <me@andrewazor.es>
2026-03-04 11:42:44 -0500

    add commit hash

--- src/main/java/org/acme/CryostatMCP.java
+++ src/main/java/org/acme/CryostatMCP.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import io.quarkiverse.mcp.server.Tool;
+import io.quarkiverse.mcp.server.ToolArg;
 import io.quarkus.qute.Qute;
 import jakarta.inject.Inject;
 
@@ -17,6 +18,7 @@ public class CryostatMCP {
         return Qute.fmt(
                 """
                 Cryostat server version: {h.cryostatVersion}
+                Git commit hash: {h.build.git.hash}
                 """,
                 Map.of("h", cryostat.health())
                 );
